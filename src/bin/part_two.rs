def5cd83197f48753fff68b02df745a17af2dcb9
lucasmolinari <lucas.molinari2003@gmail.com>
2024-04-19 15:15:55 -0300

    Remove unnecessary input test file

--- src/bin/part_two.rs
+++ src/bin/part_two.rs
@@ -24,11 +24,14 @@ mod test {
     use super::*;
 
     #[test]
-    fn part_one() {
-        let result = run("1abc2
-        pqr3stu8vwx
-        a1b2c3d4e5f
-        treb7uchet");
+    fn part_two() {
+        let result = run("two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen");
         assert_eq!(result, 142)
     }
 }
