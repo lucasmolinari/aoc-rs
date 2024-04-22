5f37374847fe7466cdc2d575f819f46943c6875b
lucasmolinari <lucas.molinari2003@gmail.com>
2024-04-22 10:00:23 -0300

    Day 1 Part 2

--- src/bin/part_two.rs
+++ src/bin/part_two.rs
@@ -7,9 +7,27 @@ fn main() {
 fn run(input: &str) -> u32 {
     let output = input
         .lines()
-        .map(|lines| {
-            let mut it = lines.chars().filter_map(|character| character.to_digit(10));
-            let first = it.next().expect("Should be u32");
+        .map(|line| {
+            let mut it = (0..line.len()).filter_map(|index| {
+                match &line[index..] {
+                    line if line.starts_with("one") => Some(1),
+                    line if line.starts_with("two") => Some(2),
+                    line if line.starts_with("three") => Some(3),
+                    line if line.starts_with("four") => Some(4),
+                    line if line.starts_with("five") => Some(5),
+                    line if line.starts_with("six") => Some(6),
+                    line if line.starts_with("seven") => Some(7),
+                    line if line.starts_with("eight") => Some(8),
+                    line if line.starts_with("nine") => Some(9),
+                    line => {
+                        line.chars().next().unwrap().to_digit(10)
+                    }
+                }
+            });
+            let first = match it.next() {
+                Some(a) => a,
+                None => panic   !("Got None"),
+            };
             match it.last() {
                 Some(n) => format!("{first}{n}").parse::<u32>(),
                 None => format!("{first}{first}").parse::<u32>(),
@@ -32,6 +50,6 @@ mod test {
         4nineeightseven2
         zoneight234
         7pqrstsixteen");
-        assert_eq!(result, 142)
+        assert_eq!(result, 281)
     }
 }
