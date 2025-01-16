75910a43d3f11811313e99d7cd923a1d3f8aaf16
lucasmolinari <lucas.molinari2003@gmail.com>
2025-01-16 17:28:07 -0300

    [2024] Day 06 Part One - Cleaner solution

--- 2024/day_06/src/bin/part_one.rs
+++ 2024/day_06/src/bin/part_one.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
 
+use util::grid::{Coord, Direction, Grid};
+
 /// Day 6: Guard Gallivant -- Part One
 fn main() {
     let input = include_str!("input.txt");
@@ -8,145 +10,48 @@ fn main() {
 }
 
 fn run(input: &str) -> usize {
-    let mut grid = Grid::new(input);
-    grid.predict()
-}
+    let lines: Vec<&str> = input.lines().collect();
+    let mut grid = Grid::new(lines[0].len() as i64, lines.len() as i64);
 
-struct Grid {
-    points: Vec<Vec<Point>>,
-    guard_pos: (usize, usize),
-}
-impl Grid {
-    fn new(input: &str) -> Self {
-        let mut guard_pos = (0, 0);
-        let points: Vec<Vec<Point>> = input
-            .lines()
-            .enumerate()
-            .map(|(y, l)| {
-                l.chars()
-                    .enumerate()
-                    .map(|(x, c)| match c {
-                        '.' => Point::Dot,
-                        '#' => Point::Wall,
-                        '<' => {
-                            guard_pos = (x, y);
-                            Point::Guard(Direction::Left)
-                        }
-                        '>' => {
-                            guard_pos = (x, y);
-                            Point::Guard(Direction::Right)
-                        }
-                        '^' => {
-                            guard_pos = (x, y);
-                            Point::Guard(Direction::Up)
-                        }
-                        'v' => {
-                            guard_pos = (x, y);
-                            Point::Guard(Direction::Down)
-                        }
-                        _ => unimplemented!(),
-                    })
-                    .collect()
-            })
-            .collect();
-        Self { points, guard_pos }
-    }
-
-    fn predict(&mut self) -> usize {
-        let mut visited = HashSet::new();
-        let max_y = self.points.len();
-        let max_x = self.points[0].len();
-        while self.guard_pos.1 < max_y && self.guard_pos.0 < max_x {
-            let (gx, gy) = (self.guard_pos.0, self.guard_pos.1);
-            visited.insert(self.guard_pos);
-
-            match self.points[gy][gx] {
-                Point::Guard(dir) => {
-                    let (nx, ny) = match dir.next_pos(gx, gy, max_x, max_y) {
-                        Some((x, y)) => (x, y),
-                        None => break,
-                    };
-                    match &self.points[ny][nx] {
-                        Point::Wall => {
-                            let nd = dir.rotate();
-                            let n = match nd.next_pos(gx, gy, max_x, max_y) {
-                                Some(n) => n,
-                                None => break,
-                            };
-                            self.update_guard((gx, gy), n, nd);
-                        }
-                        Point::Dot => self.update_guard((gx, gy), (nx, ny), dir),
-                        _ => panic!("found multiple guards"),
-                    }
-                }
-                _ => panic!(
-                    "point at guard_pos is not a guard: ({},{}) {:?}",
-                    gx, gy, self.points[gy][gx]
-                ),
-            }
+    grid.set_all(|p| {
+        let c = lines[p.y as usize]
+            .chars()
+            .nth(p.x as usize)
+            .expect("should have character");
+        match c {
+            '.' => Point::Dot,
+            '#' => Point::Wall,
+            '<' | '>' | '^' | 'v' => Point::Guard,
+            _ => unimplemented!(),
         }
-        visited.len()
-    }
-
-    fn update_guard(
-        &mut self,
-        curr_p: (usize, usize),
-        next_p: (usize, usize),
-        direction: Direction,
-    ) {
-        self.guard_pos.0 = next_p.0;
-        self.guard_pos.1 = next_p.1;
-        self.points[curr_p.1][curr_p.0] = Point::Dot;
-        self.points[next_p.1][next_p.0] = Point::Guard(direction);
-    }
+    });
+    predict(grid)
 }
 
-#[derive(Debug)]
+fn predict(grid: Grid<Point>) -> usize {
+    let mut pos = grid.find(Point::Guard).expect("should find a guard");
+    let mut dir = Coord::from(Direction::Up);
+
+    let mut visited = HashSet::new();
+    while grid.contains(pos + dir) {
+        visited.insert(pos);
+
+        let next = pos + dir;
+        if grid[next] == Point::Wall {
+            dir = dir.clockwise();
+            continue;
+        }
+
+        pos += dir;
+    }
+    visited.len() + 1
+}
+
+#[derive(Debug, PartialEq)]
 enum Point {
     Wall,
-    Guard(Direction),
+    Guard,
     Dot,
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Direction {
-    Left,
-    Right,
-    Up,
-    Down,
-}
-impl Direction {
-    fn rotate(&self) -> Self {
-        match &self {
-            Direction::Left => Direction::Up,
-            Direction::Right => Direction::Down,
-            Direction::Up => Direction::Right,
-            Direction::Down => Direction::Left,
-        }
-    }
-
-    fn next_pos(&self, x: usize, y: usize, max_x: usize, max_y: usize) -> Option<(usize, usize)> {
-        match self {
-            Direction::Left => x.checked_sub(1).map(|new_x| (new_x, y)),
-            Direction::Right => {
-                let new_x = x.checked_add(1)?;
-                if new_x < max_x {
-                    Some((new_x, y))
-                } else {
-                    None
-                }
-            }
-            Direction::Up => y.checked_sub(1).map(|new_y| (x, new_y)),
-            Direction::Down => {
-                let new_y = y.checked_add(1)?;
-                if new_y < max_y {
-                    Some((x, new_y))
-                } else {
-                    None
-                }
-            }
-        }
-    }
 }
 
 #[cfg(test)]
--- 2024/util/src/grid.rs
+++ 2024/util/src/grid.rs
@@ -1,39 +1,47 @@
-use std::fmt::{Display, Formatter};
+use std::{
+    fmt::{Display, Formatter},
+    ops::{Add, AddAssign, Index},
+};
 
 use rayon::iter::{
     IndexedParallelIterator, IntoParallelIterator, IntoParallelRefMutIterator, ParallelIterator,
 };
 
-pub struct Point {
-    pub x: usize,
-    pub y: usize,
-}
-impl Display for Point {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
-    }
-}
-
+#[derive(Debug, Default)]
 pub struct Grid<T> {
     pub data: Vec<T>,
-    pub width: usize,
-    pub height: usize,
+    pub width: i64,
+    pub height: i64,
 }
 impl<T> Grid<T> {
-    pub fn new(width: usize, height: usize) -> Self {
+    pub fn new(width: i64, height: i64) -> Self {
         Self {
-            data: Vec::with_capacity(width * height),
+            data: Vec::with_capacity((width * height) as usize),
             height,
             width,
         }
     }
 
-    pub fn get(&self, point: Point) -> &T {
-        &self.data[point.y * self.width + point.x]
+    pub fn get(&self, point: Coord) -> &T {
+        &self.data[(point.y * self.width + point.x) as usize]
     }
 
-    pub fn set(&mut self, point: Point, item: T) {
-        self.data[point.y * self.width + point.x] = item;
+    pub fn set(&mut self, point: Coord, item: T) {
+        self.data[(point.y * self.width + point.x) as usize] = item;
+    }
+
+    pub fn contains(&self, point: Coord) -> bool {
+        point.x < self.width && point.y < self.height && point.x > 0 && point.y > 0
+    }
+
+    pub fn find(&self, item: T) -> Option<Coord>
+    where
+        T: PartialEq,
+    {
+        self.data.iter().position(|it| it == &item).map(|i| Coord {
+            x: i as i64 % self.width,
+            y: i as i64 / self.width,
+        })
     }
 
     pub fn print(&self)
@@ -42,7 +50,7 @@ impl<T> Grid<T> {
     {
         for y in 0..self.height {
             for x in 0..self.width {
-                let item = self.get(Point { x, y });
+                let item = self.get(Coord { x, y });
                 print!("{}", item);
             }
             println!();
@@ -51,17 +59,82 @@ impl<T> Grid<T> {
 
     pub fn set_all<F>(&mut self, setter: F)
     where
-        F: Send + Sync + Fn(Point) -> T,
+        F: Send + Sync + Fn(Coord) -> T,
         T: Send,
     {
         self.data = (0..self.width * self.height)
             .into_par_iter()
             .map(|i| {
-                setter(Point {
+                setter(Coord {
                     x: i % self.width,
                     y: i / self.width,
                 })
             })
             .collect();
+    }
+}
+impl<T> Index<Coord> for Grid<T> {
+    type Output = T;
+
+    #[inline]
+    fn index(&self, index: Coord) -> &Self::Output {
+        &self.data[(self.width * index.y + index.x) as usize]
+    }
+}
+
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug, Default, Hash, Clone, Copy, Eq, PartialEq)]
+pub struct Coord {
+    pub x: i64,
+    pub y: i64,
+}
+impl Coord {
+    pub fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
+    }
+
+    pub fn clockwise(self) -> Self {
+        Coord::new(-self.y, self.x)
+    }
+
+    pub fn counter_clockwise(self) -> Self {
+        Coord::new(self.y, -self.x)
+    }
+}
+impl From<Direction> for Coord {
+    fn from(value: Direction) -> Self {
+        match value {
+            Direction::Up => Coord::new(0, -1),
+            Direction::Down => Coord::new(0, 1),
+            Direction::Left => Coord::new(-1, 0),
+            Direction::Right => Coord::new(1, 0),
+        }
+    }
+}
+impl Display for Coord {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+impl Add for Coord {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Coord {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+impl AddAssign for Coord {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
     }
 }
